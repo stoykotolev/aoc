116522ed7a357de5e1e19e56a6092a56c34763bf
stoykotolev <st.tolev@gmail.com>
2023-12-01 22:51:00 +0200

    feat(2023-day1): finish day 1 and setup go.

---
+++ 2023/day1/day1.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+	"unicode"
+
+	"stoykotolev/aoc-2023/utils"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+var spelledNumbers = []string{
+	"zero",
+	"one",
+	"two",
+	"three",
+	"four",
+	"five",
+	"six",
+	"seven",
+	"eight",
+	"nine",
+}
+
+func main() {
+
+	inputContents := utils.ReadFile("./input.txt")
+	// part1(inputContents)
+	part2(inputContents)
+
+}
+
+func part1(contents []string) {
+
+	total := 0
+
+	for _, line := range contents {
+		var digits []rune
+		for _, char := range line {
+			if unicode.IsDigit(char) {
+				digits = append(digits, char)
+			}
+		}
+
+		digitsLen := len(digits)
+
+		if digitsLen > 1 {
+			twoDigit := string(digits[0]) + string(digits[digitsLen-1])
+			num, err := strconv.Atoi(twoDigit)
+			check(err)
+			total = total + num
+		} else {
+			twoDigit := string(digits[0]) + string(digits[0])
+			num, err := strconv.Atoi(twoDigit)
+			check(err)
+			total = total + num
+		}
+
+	}
+	fmt.Println(total)
+}
+
+func part2(contents []string) {
+	total := 0
+	for _, line := range contents {
+		first, err := findFirst(line)
+		check(err)
+		num, err := findLast(line)
+		check(err)
+		curr := first*10 + num
+		total = total + curr
+	}
+	fmt.Println(total)
+}
+
+func findFirst(input string) (int, error) {
+	var builder strings.Builder
+
+	for _, char := range input {
+		if unicode.IsDigit(char) {
+			return int(char - '0'), nil
+		}
+
+		builder.WriteString(string(char))
+		word := builder.String()
+
+		for index, number := range spelledNumbers {
+			if strings.Contains(word, number) {
+				return index, nil
+			}
+		}
+	}
+
+	return 0, errors.New("fail")
+}
+
+func findLast(input string) (int, error) {
+	word := ""
+
+	runes := []rune(input)
+
+	for i := len(runes) - 1; i >= 0; i-- {
+		if unicode.IsDigit(runes[i]) {
+			return int(runes[i] - '0'), nil
+		}
+		word = string(runes[i]) + word
+
+		for index, number := range spelledNumbers {
+			if strings.Contains(word, number) {
+				return index, nil
+			}
+		}
+	}
+
+	return 0, errors.New("fail")
+}
